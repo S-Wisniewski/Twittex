793ab6b9fdb99027e0f3cfdbf3dc914c3abdc11e
Bartosz Rosiek <bartosz.rosiek666@gmail.com>
2026-03-28 01:33:35 +0100

    refactor models and API architecture, add new entities and update DbContext

---
+++ src/api/ModerationSystem.Api/Models/Entities/Review.cs
@@ -0,0 +1,17 @@
+﻿namespace ModerationSystem.Api.Models.Entities
+{
+    public class Review : BaseEntity
+    {
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+        // user who flagged the post
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        // reason for flagging the post
+        public string Description { get; set; }
+        // possible enum
+        public string Type { get; set; }
+    }
+}
