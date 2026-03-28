0d1f1138cdd8de334fda1d053208f1d4bab4d447
xxnls <bartosz.rosiek666@gmail.com>
2026-03-28 15:00:58 +0100

    added data annotations, reviewtype enum

--- src/api/ModerationSystem.Api/Models/Entities/BaseEntity.cs
+++ src/api/ModerationSystem.Api/Models/Entities/BaseEntity.cs
@@ -1,13 +1,15 @@
-﻿namespace ModerationSystem.Api.Models.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModerationSystem.Api.Models.Entities;
 
 public abstract class BaseEntity
 {
-    // guid?
+    [Key]
 	public int Id { get; set; }
 
+    [Required]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public DateTime? UpdatedAt { get; set; }
-
     public DateTime? DeletedAt { get; set; }
 }
