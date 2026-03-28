5d74353610d36cb231cd4c40d423f6ec48fb7762
wiziix <fexi1339@gmail.com>
2026-03-28 14:10:04 +0100

    removing fields that Cognito will handle

--- src/api/ModerationSystem.Api/Models/Entities/User.cs
+++ src/api/ModerationSystem.Api/Models/Entities/User.cs
@@ -1,19 +1,9 @@
 ﻿namespace ModerationSystem.Api.Models.Entities
 {
-    public class User : BaseEntity
+    public class User
     {
-        // role?
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public string UserName { get; set; } = string.Empty;
-        // events
+        public string CognitoUserId { get; set; } = string.Empty;
         public string AvatarUrl { get; set; } = string.Empty;
-        public bool EmailConfirmed { get; set; }
-        public bool PhoneNumberConfirmed { get; set; }
-        public bool IsBanned { get; set; }
-
-
         public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
         public virtual ICollection<PostLikes> LikedPosts { get; set; } = new List<PostLikes>();
         public virtual ICollection<UserFollows> Followers { get; set; } = new List<UserFollows>();
