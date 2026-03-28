ba9ec18cc52a779cc0b010a0cb8468ff6d857b9d
wiziix <fexi1339@gmail.com>
2026-03-28 14:30:22 +0100

    entity changes due to cognito

--- src/api/ModerationSystem.Api/Models/Entities/Post.cs
+++ src/api/ModerationSystem.Api/Models/Entities/Post.cs
@@ -4,16 +4,20 @@ namespace ModerationSystem.Api.Models.Entities;
 
 public class Post : BaseEntity
 {
-    public int UserId { get; set; }
+    public string CognitoUserId { get; set; }
+
     public virtual User User { get; set; }
 
     public int? ParentPostId { get; set; }
+
     public virtual Post? ParentPost { get; set; }
 
-    public string Content { get; set; }
+    public string Content { get; set; } = string.Empty;
+
     public PostStatus Status { get; set; } = PostStatus.Pending;
 
-    // public ICollection<Comment> Comments { get; set; } = new List<Comment>();
     public virtual ICollection<PostLikes> Likes { get; set; } = new List<PostLikes>();
+
     public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+    // Optional: public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
 }
--- src/api/ModerationSystem.Api/Models/Entities/User.cs
+++ src/api/ModerationSystem.Api/Models/Entities/User.cs
@@ -3,10 +3,21 @@
     public class User
     {
         public string CognitoUserId { get; set; } = string.Empty;
+
         public string AvatarUrl { get; set; } = string.Empty;
+
         public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+
         public virtual ICollection<PostLikes> LikedPosts { get; set; } = new List<PostLikes>();
+
         public virtual ICollection<UserFollows> Followers { get; set; } = new List<UserFollows>();
+
         public virtual ICollection<UserFollows> Following { get; set; } = new List<UserFollows>();
+
+        public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+        public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+        public virtual ICollection<Log> Logs { get; set; } = new List<Log>();
     }
 }
