c4cd01639c32ea79f0017c024186dafe6d365793
wiziix <fexi1339@gmail.com>
2026-04-11 15:08:44 +0200

    added audit service

--- src/api/ModerationSystem.Api/Program.cs
+++ src/api/ModerationSystem.Api/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using ModerationSystem.Api.Data;
 using Scalar.AspNetCore;
+using ModerationSystem.Api.Services.Audit;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IAuditService, AuditService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
---
+++ src/api/ModerationSystem.Api/Services/Audit/AuditService.cs
@@ -0,0 +1,26 @@
+using ModerationSystem.Api.Data;
+using ModerationSystem.Api.Models.Entities;
+
+namespace ModerationSystem.Api.Services.Audit;
+
+public class AuditService : IAuditService
+{
+    private readonly AppDbContext _db;
+
+    public AuditService(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task LogAsync(string cognitoUserId, string content)
+    {
+        var log = new Log
+        {
+            CognitoUserId = cognitoUserId,
+            Content = content
+        };
+
+        await _db.AddAsync(log);
+        await _db.SaveChangesAsync();
+    }
+}
