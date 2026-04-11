11b14407dc5ac23bbac08b65f838ce2c4d1f3e45
xxnls <bartosz.rosiek666@gmail.com>
2026-04-11 15:54:55 +0200

    controller

--- src/api/ModerationSystem.Api/Program.cs
+++ src/api/ModerationSystem.Api/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using ModerationSystem.Api.Data;
+using ModerationSystem.Api.Interfaces;
+using ModerationSystem.Api.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IPostService, PostService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
