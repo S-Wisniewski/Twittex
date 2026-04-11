b50f44ddcc7150cfca5f75abf617b092da6f0c4d
wiziix <fexi1339@gmail.com>
2026-04-11 15:59:27 +0200

    - standardized namespace convention - handled dotnet warnings - added initial cognito authentication, cors setup

--- src/api/ModerationSystem.Api/Models/Entities/UserFollows.cs
+++ src/api/ModerationSystem.Api/Models/Entities/UserFollows.cs
@@ -5,12 +5,14 @@ namespace ModerationSystem.Api.Models.Entities
     public class UserFollows
     {
         [Required]
-        public string FollowerId { get; set; }
-        public virtual User Follower { get; set; }
+        public string FollowerId { get; set; } = null!;
+
+        public virtual User Follower { get; set; } = null!;
 
         [Required]
-        public string FollowedId { get; set; }
-        public virtual User Followed { get; set; }
+        public string FollowedId { get; set; } = null!;
+
+        public virtual User Followed { get; set; } = null!;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
---
+++ src/api/ModerationSystem.Api/Services/Audit/IAuditService.cs
@@ -0,0 +1,8 @@
+namespace ModerationSystem.Api.Services.Audit
+{
+    public interface IAuditService
+    {
+        Task LogAsync(string cognitoUserId, string content);
+    }
+}
+
