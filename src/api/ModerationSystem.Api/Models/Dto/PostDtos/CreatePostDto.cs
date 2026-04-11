7d39d6b5aadb32f811897a1d662c682d9a509faf
xxnls <bartosz.rosiek666@gmail.com>
2026-04-11 14:33:24 +0200

    repo, dtos, mapping

---
+++ src/api/ModerationSystem.Api/Models/Dto/PostDtos/CreatePostDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ModerationSystem.Api.Models.Dto.PostDtos
+{
+    public class CreatePostDto
+    {
+        public string CognitoUserId { get; set; }
+        public int? ParentPostId { get; set; }
+        public string Content { get; set; }
+    }
+}
